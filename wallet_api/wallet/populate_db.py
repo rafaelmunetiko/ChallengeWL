b9befa92bffec31bd23419dcdaa467d754a09c78
Rafael Munetiko <munetiko96@gmail.com>
2025-01-29 14:43:42 -0300

    Sexto commit

--- wallet_api/wallet/populate_db.py
+++ wallet_api/wallet/populate_db.py
@@ -1,12 +1,8 @@
 import os
 import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wallet_project.settings')
+
+# Definir o caminho correto para o settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wallet_api.wallet_api.settings")
+
+# Configurar Django
 django.setup()
-
-from wallet.models import CustomUser, Wallet
-
-# Criar usuários fictícios
-user1 = CustomUser.objects.create_user(username='rafael', email='rafael@email.com', password='123456')
-wallet1 = Wallet.objects.create(user=user1, balance=1000.00)
-
-print("Dados populados com sucesso!")
